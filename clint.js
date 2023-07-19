f7025f9b28e3ee053db7986ed1bfedd7a12bb80b
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2023-07-19 10:35:23 +0530

    Update clint.js

--- clint.js
+++ clint.js
@@ -1,9 +1,10 @@
+// this is ther clint side
 const socket =io('http://localhost:8000');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container")
 
-
+// during run this code in localhost prompt is not running
 const name = prompt("Enter your name to join");
 socket.emit('new-user-joined', name)
